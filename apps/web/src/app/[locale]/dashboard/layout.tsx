09d990ff8121c272685ed11c24328323698fc933
Levan Beroshvili <lberoshvili9@gmail.com>
2025-12-30 03:54:39 +0400

    Make dashboard header full-width, sidebar starts below header

--- apps/web/src/app/[locale]/dashboard/layout.tsx
+++ apps/web/src/app/[locale]/dashboard/layout.tsx
@@ -10,17 +10,17 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   // For now, we'll allow access to build the UI
 
   return (
-    <div className="flex min-h-screen bg-gray-50 dark:bg-zinc-950">
-      {/* Desktop Sidebar */}
-      <DashboardSidebar />
+    <div className="min-h-screen bg-gray-50 dark:bg-zinc-950 flex flex-col">
+      {/* Header - Full width at top */}
+      <DashboardHeader />
 
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col min-w-0">
-        {/* Header with logo, burger menu (mobile), and user menu */}
-        <DashboardHeader />
+      {/* Below header: Sidebar + Content */}
+      <div className="flex flex-1">
+        {/* Desktop Sidebar */}
+        <DashboardSidebar />
 
         {/* Page Content */}
-        <main className="flex-1 p-4 lg:p-8">{children}</main>
+        <main className="flex-1 p-4 lg:p-8 min-w-0">{children}</main>
       </div>
     </div>
   );
