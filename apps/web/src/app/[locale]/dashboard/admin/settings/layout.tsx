5b93227f5981c3c7b36b8ea1ea42a47285b5c774
Levan Beroshvili <lberoshvili9@gmail.com>
2026-01-16 15:29:08 +0400

    fix build

--- apps/web/src/app/[locale]/dashboard/admin/settings/layout.tsx
+++ apps/web/src/app/[locale]/dashboard/admin/settings/layout.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from 'react';
-import { SettingsLayout } from '../../../../../../components/dashboard/admin/SettingsLayout';
+import { SettingsLayout } from '../../../../../components/dashboard/admin/SettingsLayout';
 
-export default function AdminSettingsLayout({ children }: { children: ReactNode }) {
+export default function AdminSettingsLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return <SettingsLayout>{children}</SettingsLayout>;
 }
