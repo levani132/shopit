3b00f4789e3a25532c7bca7db3a9915b35d1f657
Levan Beroshvili <lberoshvili9@gmail.com>
2026-01-19 13:09:46 +0400

    Added pm2 ecosystem config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "api",
+      cwd: "/var/www/shopit",
+      script: "apps/api/dist/main.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: "3001",
+      },
+    },
+    {
+      name: "web",
+      cwd: "/var/www/shopit/apps/web",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      env: {
+        NODE_ENV: "production",
+        PORT: "3000",
+      },
+    },
+  ],
+};
